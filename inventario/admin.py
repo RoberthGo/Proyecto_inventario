27e9928f6214c70866804d848c0cebd4a5029dd8
RoberthGo <robeg7183@gmail.com>
2026-09-22 08:19:09 -0600

    Se agregaron 2 modelos al panel de administracion (Proovedor, Ropa)

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Color, Proveedor, Ropa, Cliente
 
-# Register your models here.
+
+@admin.register(Proveedor)
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'telefono', 'correo')
+    search_fields = ('nombre', 'correo')
+
+
+@admin.register(Ropa)
+class RopaAdmin(admin.ModelAdmin):
+    list_display = ('modelo', 'tipo', 'talla', 'marca', 'precio', 'proveedor')
+    list_filter = ('tipo', 'talla', 'proveedor')
+    search_fields = ('modelo', 'marca', 'descripcion')
+
+
+admin.site.register(Color)
+admin.site.register(Cliente)
